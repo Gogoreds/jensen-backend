06de745e60b2a51451d46573acabab1f98200ee7
Mo Arefi <m-arefi@live.com>
2022-01-17 00:13:32 +0100

    Login form added

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const credentials = {secretUser:"user" , secretPassword:"password"}
 const cors = require("cors")
 const express = require("express")
-    const app = express()
+const app = express()
 
 process.env.PORT = 3000
 
 app.use(cors())
 app.get("/", (req ,res)=>{
-    res.set('WWW-Authenticate', 'Basic realm="Access to Index"')
-    res.status(401).send("Unauthorised access")
+    const encodedAuth = (req.headers.authorization || '')
+        .split(' ')[1] || '' // Getting the part after Basic
+
+    const [name, password] = Buffer.from(encodedAuth, 'base64')
+        .toString().split(':')
+        if(user===credentials.secretUser && password===credentials.secretPassword){
+        res.status(200).send({"STATUS":"SUCCESS"})
+       }else{
+        res.set('WWW-Authenticate', 'Basic realm="Access to Index"')
+        res.status(401).send("Unauthorised access")
+       }
     })
 
 app.listen(3000 , ()=>{
